11eeddf36a2f681350644ba163fe9a639052410d
Marcos Hobson <marcoshobson@gmail.com>
2020-05-01 21:52:20 -0300

    Updates

---
+++ precache-manifest.466d13e5e09ff520bc381de5de982d96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40dbf65bd3ee85bca4df98e95c87a587",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "cf13c095d9c4199f5fc4",
+    "url": "/react-gh-pages/static/css/main.1716ab64.chunk.css"
+  },
+  {
+    "revision": "8b22355cd7142fa6161c",
+    "url": "/react-gh-pages/static/js/2.9e05f188.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.9e05f188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf13c095d9c4199f5fc4",
+    "url": "/react-gh-pages/static/js/main.1a6d0311.chunk.js"
+  },
+  {
+    "revision": "93e88e3628574d6494f8",
+    "url": "/react-gh-pages/static/js/runtime-main.30b8902d.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/react-gh-pages/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
